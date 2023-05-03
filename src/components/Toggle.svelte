15b40291dc99ad807eb09761b60d5560b6d9182e
kiran-evans <thekiranevans@gmail.com>
2023-05-03 14:54:30 +0100

    Dark mode now works

---
+++ src/components/Toggle.svelte
@@ -0,0 +1,32 @@
+<script lang="ts">
+    export let label = "";
+    export let checked = false;
+    export let size = 1;
+</script>
+
+<div class="
+    flex
+    items-center
+    justify-center
+    select-none
+">
+    <input
+        type="checkbox"
+        bind:checked
+        id={label}
+        class="
+            h-[{size}rem]
+            w-[{size}rem]
+            mr-[0.2rem]
+            cursor-pointer
+        "
+    />
+
+    <label
+        for={label}
+        class="
+            leading-none
+            cursor-pointer
+        "
+    >{label}</label>
+</div>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,53 @@
 <script lang="ts">
-import "../app.css";
+	import { browser } from "$app/environment";
+	import { writable } from "svelte/store";
+	import "../app.css";
+	import Toggle from "../components/Toggle.svelte";
+    
+let checked = writable(false);
+
+$: if (browser) { $checked ? document.documentElement.classList.add('dark') : document.documentElement.classList.remove('dark') };
+
 </script>
 
-<slot />
+<div class="
+    bg-blue-100
+    text-black
+
+    dark:bg-gray-900
+    dark:text-gray-50
+
+    h-screen
+    flex
+    flex-col
+">
+
+<header class="
+    bg-blue-300
+    text-900
+
+    dark:bg-blue-900
+    dark:text-blue-200
+
+    flex
+    align-center
+    justify-center
+    flex-[1]
+    p-2
+">
+
+    <Toggle label="Dark mode" bind:checked={$checked} size={1} />
+
+</header>
+
+<main class="
+    flex-[25]
+    overflow-y-auto
+">
+
+    <slot />
+
+</main>
+
+
+</div>
